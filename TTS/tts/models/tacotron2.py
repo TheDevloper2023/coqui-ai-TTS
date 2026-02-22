84b10f65cfcd5833c9a336155403783b95bd638f
TheDevloper2023 <154549023+TheDevloper2023@users.noreply.github.com>
2026-02-22 14:33:07 +0200

    Remove leftover code from local

--- TTS/tts/models/tacotron2.py
+++ TTS/tts/models/tacotron2.py
@@ -148,19 +148,6 @@ class Tacotron2(BaseTacotron):
                 self.max_decoder_steps,
             )
 
-        # Temporary until I have good pretrain
-        # We freeze the encoder, prenet and attention from the pretrained DCA model
-        for param in self.encoder.parameters():
-            param.requires_grad = False
-
-        for param in self.decoder.prenet.parameters():
-            param.requires_grad = False
-
-        for param in self.decoder.attention.parameters():
-            param.requires_grad = False
-        
-
-
     @staticmethod
     def shape_outputs(mel_outputs, mel_outputs_postnet, alignments):
         """Final reshape of the model output tensors."""
